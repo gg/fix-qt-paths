ed8c6af4a6910f508e8c26dc3cfa2193c8daeaa4
gg <gg.github@gmail.com>
2010-11-26 12:46:45 -0600

    Refactored fix_qt_paths.bat

--- fix_qt_paths.bat
+++ fix_qt_paths.bat
@@ -9,9 +9,9 @@ if "%2" == "" (
 )
 
 if exist %QTDIR%\%QTLIBDIR% (
-set QTLIBDIR=%QTDIR%\%QTLIBDIR%
+    set QTLIBDIR=%QTDIR%\%QTLIBDIR%
 ) else if not exist %QTLIBDIR% (
-goto error_QTLIBDIR_not_found
+    goto error_QTLIBDIR_not_found
 )
 
 set QTBINDIR=%QTDIR%\bin
@@ -20,12 +20,12 @@ if not exist %QTBINDIR% goto error_QTBINDIR_not_found
 set UNXUTILSDIR=%~dp0unxutils
 if not exist %UNXUTILSDIR% goto error_unxutils_not_found
 
-@echo Fixing Qt paths...
+echo Fixing Qt paths...
 set PATH=%QTBINDIR%;%PATH%
-@echo Added %QTBINDIR% to PATH.
+echo Added %QTBINDIR% to PATH.
 
-@rem Use sed to replace QTDIR in qt.conf.template with the absolute Qt
-@rem   installation path, escaped with double backslashes.
+rem Use sed to replace QTDIR in qt.conf.template with the absolute Qt
+rem  installation path, escaped with double backslashes for Windows.
 copy %~dp0qt.conf.template %~dp0qt.conf
 set QTDIR_ESCAPED_SLASHES=%QTDIR:\=\\\\%
 set QTLIBDIR_ESCAPED_SLASHES=%QTLIBDIR:\=\\\\%
@@ -33,40 +33,43 @@ call %UNXUTILSDIR%\add-to-path
 sed -i "s'QTDIR'%QTDIR_ESCAPED_SLASHES%'g" %~dp0qt.conf
 sed -i "s'QTLIBDIR'%QTLIBDIR_ESCAPED_SLASHES%'g" %~dp0qt.conf
 
-@rem Place qt.conf alongside qmake.
+rem Place qt.conf alongside qmake.
 move %~dp0qt.conf %QTBINDIR%
 
-@echo QTDIR is set to %QTDIR%
-@echo.
-@echo qt.conf written to %QTBINDIR%
-@type %QTBINDIR%\qt.conf
-@echo.
+echo QTDIR is set to %QTDIR%
+echo.
+echo qt.conf written to %QTBINDIR%
+type %QTBINDIR%\qt.conf
+echo.
 
-@goto end
+goto end
 
+
+rem ERRORS:
 :error_Usage
-@echo Usage: %0 path\to\qt-base-directory [path\to\qt-libraries]
+echo Usage: %0 path\to\qt-base-directory [path\to\qt-libraries]
 set ERRORLEV=1
-@goto end
+goto end
 
 :error_QTDIR_not_found
-@echo ERROR: Qt directory '%QTDIR%' does not exist.
+echo ERROR: Qt directory '%QTDIR%' does not exist.
 set ERRORLEV=2
-@goto end
+goto end
 
 :error_QTLIBDIR_not_found
-@echo ERROR: Qt libraries directory '%QTLIBDIR%' does not exist.
+echo ERROR: Qt libraries directory '%QTLIBDIR%' does not exist.
 set ERRORLEV=2
-@goto end
+goto end
 
 :error_QTBINDIR_not_found
-@echo ERROR: Qt binaries directory '%QTBINDIR%' does not exist.
+echo ERROR: Qt binaries directory '%QTBINDIR%' does not exist.
 set ERRORLEV=2
-@goto end
+goto end
 
 :error_unxutils_not_found
-@echo ERROR: 'unxutils' directory does not exist.
+echo ERROR: 'unxutils' directory does not exist.
 set ERRORLEV=3
-@goto end
+goto end
+
 
 :end
